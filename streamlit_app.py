6a9cb8ee0291e865b0cbe56d115c773562d6ffa8
paolo <turatip@gmail.com>
2022-03-03 23:29:18 +0000

    add_env_var: added aws dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 
 st.write("This is the AWS ACCESS KEY ID ", AWS_ACCESS_KEY_ID)
+
 # define AWS methods
 CLIENT = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 RESOURCE = boto3.resource("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
